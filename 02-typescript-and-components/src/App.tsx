4694cfeb3a2a61faae9399b8527712bcb7bf8952
Raymond Lam <raymond.m.lam@gmail.com>
2024-04-07 10:43:31 +0200

    lesson-2 project

---
+++ 02-typescript-and-components/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Heading from "./heading";
+
+function App() {
+	return (
+		<>
+			<Heading />
+			<span className="hand">👋</span>
+			<p className="greeting">My name is Raymond</p>
+		</>
+	);
+}
+
+export default App;
---
+++ 02-typescript-and-components/src/heading.tsx
@@ -0,0 +1,5 @@
+function Heading() {
+	return <h1 className="heading">Hello</h1>;
+}
+
+export default Heading;
